f1a81bcfe20f6dfa3333cc2df9d896cdf8c1f3d5
PUTTIPONG3035 <88504764+PUTTIPONG3035@users.noreply.github.com>
2025-02-09 03:28:09 +0700

    backend

---
+++ backend/api/controller/ProductController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+public static class ProductEndpoints
+{
+    public static void ProductController(this WebApplication app, string connectionString)
+    {
+        app.MapGet("/product", async () =>
+        {
+            using var connection = new SqlConnection(connectionString);
+            var result = await connection.QueryAsync<Product>("SELECT * FROM Product");
+            return Results.Ok(result);
+        })
+        .WithName("GetProductData")
+        .WithOpenApi();
+
+        app.MapGet("/product/{id}", async (int id) =>{
+            using var connection = new SqlConnection(connectionString);
+            var result = await connection.QueryFirstAsync<Product>($"SELECT * FROM Product WHERE ProductId = {id}");
+            return Results.Ok(result);
+        })
+        .WithName("GetProductById")
+        .WithOpenApi();
+        
+       app.MapPost("/product", async (Product product) =>{
+         using var connection = new SqlConnection(connectionString);
+         await connection.ExecuteAsync("INSERT INTO Product (ProductName, Price, Rating, ProductImage, Date) VALUES (@ProductName, @Price, @Rating, @ProductImage, @Date)", product);
+         return Results.Created($"/product/{product.ProductId}", product);
+       })
+       .WithName("CreateProduct")
+       .WithOpenApi();
+
+    }
+}
